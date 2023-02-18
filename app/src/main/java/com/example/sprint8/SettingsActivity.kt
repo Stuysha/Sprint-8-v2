f23c92527e1b7907d57d4d79a9580cff013d987a
Stuysha <styusha.kinski@yandex.ru>
2023-02-18 19:01:40 +0300

    Спринт 9 ДЗ №2 4/4

--- app/src/main/java/com/example/sprint8/SettingsActivity.kt
+++ app/src/main/java/com/example/sprint8/SettingsActivity.kt
@@ -16,15 +16,12 @@ class SettingsActivity : AppCompatActivity() {
         buttonArrow.setOnClickListener {
             onBackPressed()
         }
-
-
         val buttonShareApp = findViewById<FrameLayout>(R.id.share_app)
         buttonShareApp.setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND)
             intent.type = "text/plain"
             intent.putExtra(
-                Intent.EXTRA_TEXT,
-                getString(R.string.share_app_step2)
+                Intent.EXTRA_TEXT, getString(R.string.share_app_step2)
             )
             startActivity(intent)
         }
@@ -33,18 +30,23 @@ class SettingsActivity : AppCompatActivity() {
             val message = getString(R.string.text_message)
             val topic = getString(R.string.topic_message)
             val email = getString(R.string.e_mail)
-
-
-
             val intent = Intent(Intent.ACTION_SENDTO).apply {
-                data = Uri.parse("mailto:") // only email apps should handle this
+                data = Uri.parse("mailto:")
                 putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
                 putExtra(Intent.EXTRA_SUBJECT, topic)
                 putExtra(Intent.EXTRA_TEXT, message)
             }
             startActivity(intent)
-             }
         }
 
+        val buttonUserAgreement = findViewById<FrameLayout>(R.id.uUser_agreement)
+        buttonUserAgreement.setOnClickListener {
+            val webesite = getString(R.string.webesite)
+            val webpage: Uri = Uri.parse(webesite)
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+                startActivity(intent)
+        }
     }
+
 }
+
