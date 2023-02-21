09e1313e8775c265825d0d3a6f7ccd3258d3504c
Stuysha <styusha.kinski@yandex.ru>
2023-02-21 14:54:42 +0300

    Спринт 9 ДЗ №3

--- app/src/main/java/com/example/sprint8/SerchActivity.kt
+++ app/src/main/java/com/example/sprint8/SerchActivity.kt
@@ -1,11 +1,46 @@
 package com.example.sprint8
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class SerchActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_serch)
+
+        val closeSearch = findViewById<TextInputEditText>(R.id.search)
+        val inputEditText = findViewById<TextInputEditText>(R.id.search)
+        val clearButton = findViewById<ImageView>(R.id.close)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+        clearButton.visibility = View.GONE
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()){
+                    clearButton.visibility = View.GONE
+                } else {
+                    clearButton.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+        }
+
+        inputEditText.addTextChangedListener(simpleTextWatcher)
     }
 }
